598304473b4ffde18fa08c115e14a1d2630cbefc
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-03-30 13:12:29 -0500

    added components for home

---
+++ client/src/components/userInfo/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function userInfo() {
+return <section className="hero is-warning is-bold">
+<div className="hero-body">
+  <div className="columns is-mobile ">
+    <div className="column is-one-fifth">
+      <figure className="image is-128x128">
+        <img className="is-rounded" id="user_icon" src="https://bulma.io/images/placeholders/128x128.png"></img>
+      </figure>
+    </div>
+    <div className="column is-four-fifths">
+      <div className="container">
+        <div className="container animation">
+          <div className="animate seven">
+            <span>D</span><span>i</span><span>n</span><span>D</span><span>r</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+}
+
+export default userInfo; 
