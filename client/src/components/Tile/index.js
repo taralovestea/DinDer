cc1c78d8c6696d408468a2f79b8fb359dc924420
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-04-02 11:05:17 -0500

    added props.profilePic to appropriate spot on UserTile

--- client/src/components/Tile/index.js
+++ client/src/components/Tile/index.js
@@ -6,17 +6,15 @@ function UserTile(props) {
       <article className="tile is-child notification has-background-grey-lighter">
 
         <figure className="image is-128x128">
-          <img className="is-rounded" id="user_icon" src="images/taralovestea.png"></img>
+          <img className="is-rounded" id="user_icon" src={props.profilePic}></img>
         </figure>
-
-uploadImg
-          <p className="title">{props.name}</p>
-          <p className="subtitle">{props.header}</p>
-          <p> <b>Stats:</b></p>
+        <p className="title">{props.name}</p>
+        <p className="subtitle">{props.header}</p>
+        <p> <b>Stats:</b></p>
         <p>Dedication Level: <i className="fas fa-award"></i>{props.dedication}</p>
-        Dungeon Master: {props.isMaster} <br/>
-        Campaign(s): {props.campaigns} <br/>
-        Experience: {props.experience} <br/>
+        Dungeon Master: {props.isMaster} <br />
+        Campaign(s): {props.campaigns} <br />
+        Experience: {props.experience} <br />
 
       </article>
     </div>
