1cb8aa4a1ada8830248ab46611a48f449c42d457
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-04-06 13:29:07 -0500

    changed route

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ const apiRoutes = require("./api");
 router.use(apiRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+
 
 module.exports = router;
