d45bf4284368e7654f535a046f265e72c4677471
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-04-06 12:48:51 -0500

    added edit profile button

--- configs/connection.js
+++ configs/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "project3"
-});
+})};
 
 connection.connect(function(err) {
   if (err) {
